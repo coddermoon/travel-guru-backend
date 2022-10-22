991f874b35144dbedaa6d7ffe22c8c5f6bc88bf7
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-22 23:48:16 +0600

    done backend server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const port = process.env.PORT ||5000
+const places = require('./data/places.json')
 
 const app = express()
 
@@ -9,6 +10,16 @@ app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('wow homepage  of this server is running')
+})
+
+app.get('/places',(req,res)=>{
+    res.send(places)
+})
+
+app.get('/places/:id',(req,res)=>{
+const id = req.params.id;
+const selectedPlace = places.find(place=> id== place.id)
+res.send(selectedPlace)
 })
 
 app.listen(port,()=>{
